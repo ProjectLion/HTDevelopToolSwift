45b4027e737891e1b13cd4dd916601aef01ca792
黄涛 <huangtao@eschervr.com>
2018-05-31 17:30:59 +0800

    在自定义控件中新增加: 新用户引导高亮视图控件 在otherTools中新增加: 私有权限工具类

---
+++ HTDevelopToolsSwift/OtherTools/私有权限的获取/HTPrivatePermission.swift
@@ -0,0 +1,112 @@
+//
+//  HTPrivatePermission.swift
+//  HTDevelopTool_Swift
+//
+//  Created by Ht on 2018/5/31.
+//  Copyright © 2018年 HT. All rights reserved.
+//
+
+import UIKit
+import Photos
+
+enum HTPhotoPermissionType {
+    /// 已经获取到了
+    case geted
+    /// 没有权限 or 其他情况
+    case other
+}
+
+/// 私有权限的获取 需要在info.plist文件中配置相关的权限
+class HTPrivatePermission: NSObject {
+    
+    /// 获取单例
+    static let shared = HTPrivatePermission()
+    private override init() {
+        
+    }
+    
+}
+//MARK: ^^^^^^^^^^^^^^^ getPermissions ^^^^^^^^^^^^^^^
+extension HTPrivatePermission {
+    /// 主动获取摄像头权限 系统会主动弹出提示框
+    func getVideoPermission(status: @escaping (Bool) -> Void) {
+        AVCaptureDevice.requestAccess(for: .video) { (statu) in
+            status(statu)
+        }
+    }
+    
+    /// 主动获取麦克风权限 系统会主动弹出提示框
+    func getAudioPermission(status: @escaping (Bool) -> Void) {
+        AVCaptureDevice.requestAccess(for: .audio) { (statu) in
+            status(statu)
+        }
+    }
+    
+    /// 获取相册的写状态权限 如果从未获取过会主动去获取,状态通过闭包返回 如果曾经获取过了会通过闭包返回对应的状态
+    func getPhotoPermission(status: @escaping (HTPhotoPermissionType) -> Void) {
+        let status = PHPhotoLibrary.authorizationStatus()
+        switch status {
+        case .authorized:   //已经获取到权限
+            status(.geted)
+        case .notDetermined:    //从未获取过权限
+            PHPhotoLibrary.requestAuthorization({ (statu) in
+                if statu == .authorized {
+                    status(.geted)
+                }else{
+                    status(.other)
+                }
+            })
+        default:
+            status(.other)
+        }
+    }
+}
+
+//MARK: ^^^^^^^^^^^^^^^ checkPermissions ^^^^^^^^^^^^^^^
+extension HTPrivatePermission {
+    /// 检查当前相机、相册、麦克风权限
+    ///
+    /// - Returns: 是否获取全部权限
+    func checkPermissions() -> Bool{
+        return checkVideo() && checkPhoto() && checkAudio()
+    }
+    
+    /// 检查相机权限
+    ///
+    /// - Returns: 是否获得权限
+    func checkVideo() -> Bool{
+        let status = AVCaptureDevice.authorizationStatus(for: .video)
+        switch status {
+        case .authorized:
+            return true
+        default:
+            return false
+        }
+    }
+    
+    /// 检查相册权限
+    ///
+    /// - Returns: 是否获得权限
+    func checkPhoto() -> Bool{
+        let status = PHPhotoLibrary.authorizationStatus()
+        switch status {
+        case .authorized:
+            return true
+        default:
+            return false
+        }
+    }
+    
+    /// 检查麦克风权限
+    ///
+    /// - Returns: 是否获得权限
+    func checkAudio() -> Bool{
+        let status = AVCaptureDevice.authorizationStatus(for: .audio)
+        switch status {
+        case .authorized:
+            return true
+        default:
+            return false
+        }
+    }
+}
---
+++ HTDevelopToolsSwift/自定义控件/新用户引导高亮/HTUserGuideHighLightView.swift
@@ -0,0 +1,64 @@
+//
+//  HTUserGuideHighLightView.swift
+//  HTDevelopTool_Swift
+//
+//  Created by Ht on 2018/5/31.
+//  Copyright © 2018年 HT. All rights reserved.
+//
+
+import UIKit
+
+open enum HTUserGuideStyle {
+    /// 正方形
+    case square
+    /// 长方形
+    case rectangle
+    /// 椭圆
+    case ellipse
+    /// 圆形
+    case circle
+    /// 其他
+    case other
+}
+
+class HTUserGuideHighLightView: UIView {
+    
+    /// 灰色部分的透明度
+    open var grayAlpha: CGFloat = 0.4 {
+        didSet {
+            self.alpha = grayAlpha
+        }
+    }
+    /// 高亮部分的风格
+    open var style: HTUserGuideStyle {
+        didSet {
+            
+        }
+    }
+    
+    
+    private var selfStyle: HTUserGuideStyle = .rectangle
+    
+    convenience init(lightFrame: CGRect) {
+        self.init(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height))
+        
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
+
+//MARK: ^^^^^^^^^^^^^^^ 公开的属性 ^^^^^^^^^^^^^^^
+extension HTUserGuideHighLightView {
+    
+}
